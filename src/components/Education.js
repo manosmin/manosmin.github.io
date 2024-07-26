7e07e505d690c85f497aa4a7fde35cc7bd94aa43
manosmin <manosmin@hotmail.com>
2024-07-27 01:50:04 +0300

    changed education dates

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Education() {
   return (
     <section id="education" className="bg-gray-900 text-slate-200 p-10">
-      <h2 className=" mb-4 text-2xl font-semibold">Education</h2>
-      <a rel="noopener noreferrer" href="https://www.ceid.upatras.gr" target='_blank' className="font-semibold text-slate-200 hover:underline">Computer Engineering and Informatics Department, University of Patras</a><p>2014 - 2024</p>
+      <h2 className="mb-4 text-2xl font-semibold">Education</h2>
+      <a rel="noopener noreferrer" href="https://www.ceid.upatras.gr" target='_blank' className="font-semibold text-slate-200 hover:underline">Computer Engineering and Informatics Department, University of Patras</a><p>09/2014 &ndash; 07/2024</p>
       <p className="text-slate-200 inline font-medium">Grade: </p> <p className="text-slate-200 inline">6.57/10.0 (Lower Second Class Honours)</p><br/>
       <p className="text-slate-200 inline font-medium">Thesis: </p> <a rel="noopener noreferrer" target="_blank" href="https://nemertes.library.upatras.gr/items/b24439d5-3bd7-4de0-b9f6-8f67c0ddf3aa"className="hover:underline inline">"Process mining on cyber-physical systems: Liqueur Plant case study" </a><p className="text-slate-200 italic inline">(Integration of a data-driven methodology named "process mining" on a liqueur production system in order to reveal, analyze and find ways to improve the production process)</p>
     </section>
