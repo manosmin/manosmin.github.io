72c325433927a1a9360564bf8ae420e010f031a9
manosmin <manosmin@hotmail.com>
2024-07-27 04:30:43 +0300

    sticky footer always at bottom

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex flex-col min-h-screen">
       <Header />
       <About />
       <Education />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-gray-200 p-5">
-      <p>I made this web page using <a rel="noopener noreferrer" href="https://www.react.dev/" target="_blank"
+    <footer className="mt-auto sticky bottom-0 bg-gray-900 text-gray-200 p-5 ">
+      <p>&copy; 2024 Manos Minadakis. I made this web page using <a rel="noopener noreferrer" href="https://www.react.dev/" target="_blank"
       className="text-gray-200 hover:underline">React.js</a> and <a rel="noopener noreferrer" href="https://tailwindcss.com/" target="_blank"
-      className="text-gray-200 hover:underline">Tailwind CSS</a> &copy; 2024 Manos Minadakis </p>
+      className="text-gray-200 hover:underline">Tailwind CSS</a>.</p>
     </footer>
   );
 };
