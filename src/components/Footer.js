fbec20b2e3e10deda72d558cd728d9dd5827b510
manosmin <manosmin@hotmail.com>
2024-07-28 01:54:31 +0300

    removed dark mode from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,11 +15,8 @@ const Footer = () => {
     <footer className="mt-auto flex justify-between bg-gray-900 text-gray-200 dark:bg-gray-200 dark:text-gray-900 p-5 ">
       <p className='inline'>I built this web page using <a rel="noopener noreferrer" href="https://www.react.dev/" target="_blank"
       className="hover:underline">React.js</a> and <a rel="noopener noreferrer" href="https://tailwindcss.com/" target="_blank"
-      className="hover:underline">Tailwind CSS</a> &copy; Manos Minadakis 2024</p>
-      <button className="inline" onClick={()=> darkModeHandler()}>
-        { dark && <IoSunny size={25}/> }
-        { !dark && <IoMoon size={25}/> }
-      </button>
+      className="hover:underline">Tailwind CSS.</a></p>
+      <p className='inline'> &copy; Manos Minadakis 2024</p>
     </footer>
   );
 };
