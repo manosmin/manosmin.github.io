9f00a697ce302543803ac8b1c7aa737d33ced905
manosmin <manosmin@hotmail.com>
2024-07-27 17:02:01 +0300

    added project items

---
+++ src/components/ProjectItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ProjectItem(props) {
+  return (
+    <ul className='mt-4 list-none'>
+        <li><a rel="noopener noreferrer"
+        target="_blank"
+        href={props.myInfo.Link}
+        className="font-semibold hover:underline">{props.myInfo.Title}</a></li>
+        <li >{props.myInfo.Date}</li>
+        <li className='italic'>{props.myInfo.Description}</li>
+        <li className='italic'>Used {props.myInfo.Used}</li>
+    </ul>
+  )
+}
+
+export default ProjectItem
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
+import ProjectItem from './ProjectItem';
 import { MdComputer } from "react-icons/md";
-
 
 function Projects() {
     const [isOpen, setIsOpen] = useState(false);
@@ -8,6 +8,31 @@ function Projects() {
     const toggleAccordion = () => {
       setIsOpen(!isOpen);
     };
+
+
+    const ProjectInfo1 = {
+      Title: 'Movie Picker Web App',
+      Used: 'React.js, Bootstrap 5',
+      Description: 'A web application that uses a REST API to provide a search engine for movies, games, series etc.',
+      Link: 'https://github.com/manosmin/movie-picker-app',
+      Date: '02/2024'
+    }
+
+    const ProjectInfo2 = {
+      Title: 'Virus Contact Tracing Website',
+      Used: 'Node.js, Express.js, Passport.js, Leaflet.js, Chart.js, Bootstrap 3, MongoDB',
+      Description: 'A website that helps users track and manage their potential exposure to virus carriers by crowd-sourcing data about their visits to various places in Patras, Greece.',
+      Link: 'https://github.com/manosmin/ceid-web',
+      Date: '05/2022'
+    }
+
+    const ProjectInfo3 = {
+      Title: 'manosmin.github.io',
+      Used: 'React.js, Tailwind CSS',
+      Description: 'A fully responsive web page used as a personal portfolio, in order to showcase my education, experience, projects etc.',
+      Link: 'https://github.com/manosmin/manosmin.github.io',
+      Date: '07/2024'
+    }
 
   return (
     <section id="projects" className="bg-gray-900 text-gray-200 p-10">
@@ -17,11 +42,11 @@ function Projects() {
       >
         <MdComputer className='inline'/> Projects
       </button>
-      {isOpen && (
-        <p>Check my profile on <a rel="noopener noreferrer"
-        target="_blank"
-        href="https://www.github.com/manosmin"
-        className="font-medium hover:underline">GitHub</a> 🔗</p>
+      {isOpen && (<div>
+        <ProjectItem myInfo={ProjectInfo1}></ProjectItem>
+        <ProjectItem myInfo={ProjectInfo2}></ProjectItem>
+        <ProjectItem myInfo={ProjectInfo3}></ProjectItem>
+        </div>
         )}
     </section>
   )
