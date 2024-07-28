e615825ebd78a9788053586bb13735a0beb24f3b
manosmin <manosmin@hotmail.com>
2024-07-28 18:52:15 +0300

    added projects

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -5,11 +5,11 @@ import { FiExternalLink } from "react-icons/fi";
 function ProjectItem(props) {
   return (
     <ul className='mt-4 list-none'>
-        <li>
+        <li>{props.myInfo.Icon}&nbsp;<FiExternalLink className='inline'/>&nbsp;
         <a rel="noopener noreferrer"
         target="_blank"
         href={props.myInfo.Link}
-        className="font-semibold hover:underline"><FiExternalLink className='inline'/> {props.myInfo.Title}</a></li>
+        className="font-semibold hover:underline">{props.myInfo.Title}</a></li>
         <li >{props.myInfo.Date}</li>
         <li className=''>{props.myInfo.Description}</li>
         <li className='italic'>Used {props.myInfo.Used}</li>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import ProjectItem from './ProjectItem';
-import { MdComputer } from "react-icons/md";
+import { MdComputer, MdPerson } from "react-icons/md";
+import { FaUniversity } from "react-icons/fa";
+
 
 function Projects() {
     const [isOpen, setIsOpen] = useState(false);
@@ -15,7 +17,8 @@ function Projects() {
       Used: 'React.js, Bootstrap 5',
       Description: 'A web application that uses a REST API to provide a search engine for movies, games, series etc.',
       Link: 'https://github.com/manosmin/movie-picker-app',
-      Date: '02/2024'
+      Date: '02/2024',
+      Icon: <MdPerson className='inline' />
     }
 
     const ProjectInfo1 = {
@@ -23,7 +26,8 @@ function Projects() {
       Used: 'Node.js, Express.js, Passport.js, Leaflet.js, Chart.js, Bootstrap 3, MongoDB',
       Description: 'A website that helps users track and manage their potential exposure to virus carriers by crowd-sourcing data about their visits to various places in Patras, Greece.',
       Link: 'https://github.com/manosmin/ceid-web',
-      Date: '05/2022'
+      Date: '05/2022',
+      Icon: <FaUniversity className='inline' />
     }
 
     const ProjectInfo3 = {
@@ -31,8 +35,28 @@ function Projects() {
       Used: 'React.js, Tailwind CSS',
       Description: 'A fully responsive web page used as a personal portfolio, in order to showcase my education, experience, projects etc.',
       Link: 'https://github.com/manosmin/manosmin.github.io',
-      Date: '07/2024'
+      Date: '07/2024',
+      Icon: <MdPerson className='inline' />
     }
+
+    const ProjectInfo4 = {
+      Title: 'Decentralized Systems Implementation',
+      Used: 'Python',
+      Description: 'Implementation of chord and pastry distributed hashing table algorithms (DHTs) and experimental evaluation using real data sets scraped from the web.',
+      Link: 'https://github.com/CPP-Bot-78/Decentralized-Systems-Technologies',
+      Date: '12/2023',
+      Icon: <FaUniversity className='inline' />
+    }
+
+    const ProjectInfo5 = {
+      Title: 'Advanced Microprocessors Programming',
+      Used: 'Assembly, C++',
+      Description: 'Programming of an 8-bit AVR microcontroller to operate various smart tasks as highway intersection traffic lights, household appliance moving in empty spaces and house alarm system.',
+      Link: 'https://github.com/manosmin/ceid-advanced-microprocessors',
+      Date: '05/2021',
+      Icon: <FaUniversity className='inline' />
+    }
+
 
   return (
     <section id="projects" className="bg-gray-900 text-gray-200 dark:bg-gray-200 dark:text-gray-900 p-10">
@@ -42,11 +66,14 @@ function Projects() {
       >
         <MdComputer className='inline'/> Projects
       </button>
-      {isOpen && (<div>
+      {isOpen && (<>
+      <p className='italic mt-4'> In my university years, I've implemented several different projects together with my fellow students. From Data Science, Machine Learning and Algorithm Implementations in Python, to Full Stack Web Development in JavaScript, HTML, CSS and Microcontrollers Programming in Assembly and C++. Below you can see some major projects from my studies and some personal ones. </p>
         <ProjectItem myInfo={ProjectInfo3}></ProjectItem>
         <ProjectItem myInfo={ProjectInfo2}></ProjectItem>
+        <ProjectItem myInfo={ProjectInfo4}></ProjectItem>
         <ProjectItem myInfo={ProjectInfo1}></ProjectItem>
-        </div>
+        <ProjectItem myInfo={ProjectInfo5}></ProjectItem>
+        </>
         )}
     </section>
   )
