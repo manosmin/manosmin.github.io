7d019d04aa88f3b7cc0e8c6fc635ba48f147a247
manosmin <manosmin@hotmail.com>
2024-07-30 17:38:56 +0300

    fixed headings delay

--- src/components/Education.js
+++ src/components/Education.js
@@ -11,7 +11,7 @@ const Education = () => {
   return (
     <div className='bg-gray-900 text-gray-200 dark:bg-gray-200 dark:text-gray-900 p-10 mb-10'>
       <div id="education" onClick={toggleAccordion} className="cursor-pointer">
-        <h2 className="animate-slide-in opacity-0 text-2xl font-semibold" style={{ "--delay": 0.2 + "s" }}>
+        <h2 className="animate-slide-in opacity-0 text-2xl font-semibold" style={{ "--delay": 0.4 + "s" }}>
           <FiBookOpen className='inline'/> Education
         </h2>
       </div>
