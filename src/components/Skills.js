a07f206fb3501063f75865580aed6766ffb6c824
manosmin <manosmin@hotmail.com>
2024-08-07 13:27:31 +0300

    changed skill percentages

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -19,14 +19,14 @@ function Skills() {
     const Skill2 = {Title: 'HTML', Percentage: '80%', Icon: <FaHtml5 className='mb-2 inline' size={25} />}
     const Skill3 = {Title: 'CSS', Percentage: '80%', Icon: <FaCss3Alt className='mb-2 inline' size={25} />}
     const Skill4 = {Title: 'PHP', Percentage: '60%', Icon: <SiPhp className='mb-2 inline' size={25} />}
-    const Skill5 = {Title: 'MongoDB', Percentage: '70%', Icon: <SiMongodb className='mb-2 inline' size={25} />}
-    const Skill6 = {Title: 'SQL', Percentage: '80%', Icon: <FaDatabase className='mb-2 inline' size={25} />}
-    const Skill7 = {Title: 'Python', Percentage: '80%', Icon: <FaPython className='mb-2 inline' size={25} />}
-    const Skill8 = {Title: 'C++', Percentage: '60%', Icon: <SiCplusplus className='mb-2 inline' size={25} />}
+    const Skill5 = {Title: 'MongoDB', Percentage: '75%', Icon: <SiMongodb className='mb-2 inline' size={25} />}
+    const Skill6 = {Title: 'SQL', Percentage: '75%', Icon: <FaDatabase className='mb-2 inline' size={25} />}
+    const Skill7 = {Title: 'Python', Percentage: '75%', Icon: <FaPython className='mb-2 inline' size={25} />}
+    const Skill8 = {Title: 'C++', Percentage: '65%', Icon: <SiCplusplus className='mb-2 inline' size={25} />}
     const Skill9 = {Title: 'Java', Percentage: '50%', Icon: <FaJava className='mb-2 inline' size={25} />}
     const Skill10 = {Title: 'Kotlin', Percentage: '50%', Icon: <SiKotlin className='mb-2 inline' size={25} />}
-    const Skill11 = {Title: 'Git', Percentage: '80%', Icon: <FaGitAlt className='mb-2 inline' size={25} />}
-    const Skill12 = {Title: 'Bash', Percentage: '60%', Icon: <SiGnubash className='mb-2 inline' size={25} />}
+    const Skill11 = {Title: 'Git', Percentage: '75%', Icon: <FaGitAlt className='mb-2 inline' size={25} />}
+    const Skill12 = {Title: 'Bash', Percentage: '65%', Icon: <SiGnubash className='mb-2 inline' size={25} />}
     const Skills = [Skill1, Skill2, Skill3, Skill4, Skill5, Skill6, Skill7, Skill8, Skill9, Skill10, Skill11, Skill12]
     
   return (
