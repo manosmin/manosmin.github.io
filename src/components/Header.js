44ef698bb899db0c4a521288986d32bb52aa08e5
manosmin <manosmin@hotmail.com>
2024-08-10 23:27:05 +0300

    seo optimization

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,11 +20,11 @@ const Header = () => {
 
   return (
     <header className="z-40 sticky top-0 bg-gray-200 text-gray-900 dark:bg-gray-900 dark:text-gray-200 shadow p-5">
-      <h2 className="max-w-screen-xl mx-auto w-full text-4xl font-bold">
+      <h1 className="max-w-screen-xl mx-auto w-full text-4xl font-bold">
         <a href="/#" className='inline-block animate-slide-in-left opacity-0' style={{ "--delay": 0.2 + "s" }}>Manos Minadakis</a>
         &nbsp;
         <small className="inline-block animate-slide-in opacity-0 font-semibold text-gray-600 dark:text-gray-400" style={{ "--delay": 0.2 + "s" }}>Computer Engineer</small>
-      </h2>
+      </h1>
       <nav>
         <ul className="max-w-screen-xl mx-auto w-full flex justify-between mt-3 list-none">
           <div className='flex animate-drop-in opacity-0' style={{ "--delay": 0.8 + "s" }}>
--- src/components/LettersAnimation.js
+++ src/components/LettersAnimation.js
@@ -3,9 +3,9 @@ import React from 'react'
 function LettersAnimation() {
   return (
     <div className="flex justify-center items-center bg-gray-200 text-gray-900 dark:bg-gray-900 dark:text-gray-200 h-screen">
-      <h1 class="bottom-48 text-4xl font-semibold relative w-[max-content] before:absolute before:inset-0 before:animate-typewriter before:bg-gray-200 dark:before:bg-gray-900 after:absolute after:inset-0 after:w-[0.175em] after:animate-caret after:bg-gray-900 dark:after:bg-gray-200">Hello!&nbsp;
+      <h2 class="bottom-48 text-4xl font-semibold relative w-[max-content] before:absolute before:inset-0 before:animate-typewriter before:bg-gray-200 dark:before:bg-gray-900 after:absolute after:inset-0 after:w-[0.175em] after:animate-caret after:bg-gray-900 dark:after:bg-gray-200">Hello!&nbsp;
         <p class="inline-block">I'm <span className='font-bold'>Manos</span>.</p>
-      </h1>
+      </h2>
     </div>
   )
 }
