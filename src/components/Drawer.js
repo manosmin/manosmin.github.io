795333d9c671c6c7f1c99037a9f82ab4490377e4
manosmin <manosmin@hotmail.com>
2024-08-24 02:36:24 +0300

    menu shadow

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -9,10 +9,10 @@ import { IoClose } from "react-icons/io5";
 function Drawer({ toggleDropdown, isOpen }) {
     
   return (
-<div id="drawer-navigation" class={`opacity-0 ${isOpen ? 'md:animate-slide-in animate-fade-in-down': 'md:animate-slide-out animate-fade-out-down'} fixed top-0 right-0 z-50 h-screen p-4 bg-gray-200 md:w-72 w-1/2 w-screen dark:bg-gray-800`}aria-labelledby="drawer-navigation-label" style={{ "--delay": 0 + "s" }}>
+<div id="drawer-navigation" class={`opacity-0 ${isOpen ? 'md:animate-slide-in animate-fade-in-down': 'md:animate-slide-out animate-fade-out-down'} fixed top-0 right-0 z-50 h-screen shadow p-4 bg-gray-200 md:w-72 w-screen dark:bg-gray-800`}aria-labelledby="drawer-navigation-label" style={{ "--delay": 0 + "s" }}>
         <h5 id="drawer-navigation-label" class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">Contents</h5>
-        <button type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" class="text-gray-500 dark:text-gray-400 hover:text-gray-900 bg-transparent hover:bg-gray-100  rounded-lg text-sm w-8 h-8 absolute top-3.5 end-3.5 2 inline-flex items-center justify-center dark:hover:bg-gray-700 dark:hover:text-white" onClick={toggleDropdown}>
-        <IoClose/>
+        <button type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" class="text-gray-500 dark:text-gray-400 hover:text-gray-900 bg-transparent hover:bg-gray-100  rounded-lg w-8 h-8 absolute top-3 end-3 inline-flex items-center justify-center dark:hover:bg-gray-700 dark:hover:text-white" onClick={toggleDropdown}>
+        <IoClose size={20}/>
         </button>
         <div class="py-4 overflow-y-auto">
             <ul class="space-y-2 font-medium" onClick={toggleDropdown}>
