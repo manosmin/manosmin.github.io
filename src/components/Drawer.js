8a99bffe0f70276706bac290942ad7a5a0beedf1
manosmin <manosmin@hotmail.com>
2024-08-24 22:23:04 +0300

    moved icons to drawer

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,61 +1,120 @@
 import React from 'react'
-import { FiBookOpen, FiInfo} from "react-icons/fi";
-import { FaRegGrinStars, FaRegEnvelope } from "react-icons/fa";
-import { MdComputer } from "react-icons/md";
+import { FiBookOpen, FiInfo } from "react-icons/fi";
+import { FaRegGrinStars, FaRegEnvelope, FaGithub, FaLinkedinIn, FaDiscord } from "react-icons/fa";
+import { MdComputer, MdAlternateEmail } from "react-icons/md";
 import { GoGear } from "react-icons/go";
-import { IoClose } from "react-icons/io5";
-
+import { IoClose, IoDocument } from "react-icons/io5";
 
 function Drawer({ toggleDropdown, isOpen }) {
-    
   return (
-<div id="drawer-navigation" class={`opacity-0 ${isOpen ? 'md:animate-slide-in animate-fade-in-down': 'md:animate-slide-out animate-fade-out-down'} fixed top-0 right-0 z-50 h-screen shadow p-4 bg-gray-200 md:w-72 w-screen dark:bg-gray-800`}aria-labelledby="drawer-navigation-label" style={{ "--delay": 0 + "s" }}>
-        <h5 id="drawer-navigation-label" class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">Contents</h5>
-        <button type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" class="text-gray-500 dark:text-gray-400 hover:text-gray-900 bg-transparent hover:bg-gray-100  rounded-lg w-8 h-8 absolute top-3 end-3 inline-flex items-center justify-center dark:hover:bg-gray-700 dark:hover:text-white" onClick={toggleDropdown}>
-        <IoClose size={20}/>
-        </button>
-        <div class="py-4 overflow-y-auto">
-            <ul class="space-y-2 font-medium" onClick={toggleDropdown}>
-                <li>
-                    <a href="#about" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <FiInfo />
-                    <span class="ms-3">About Me</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#education" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <FiBookOpen/>
-                    <span class="flex-1 ms-3 whitespace-nowrap">Education</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#projects" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <MdComputer/>
-                    <span class="flex-1 ms-3 whitespace-nowrap">Projects</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#skills" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <GoGear/>
-                    <span class="flex-1 ms-3 whitespace-nowrap">Skills</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#facts" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <FaRegGrinStars/>
-                    <span class="flex-1 ms-3 whitespace-nowrap">Fun Facts</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#contact" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <FaRegEnvelope/>
-                    <span class="flex-1 ms-3 whitespace-nowrap">Contact</span>
-                    </a>
-                </li>
+    <div id="drawer-navigation" className={`opacity-0 ${isOpen ? 'md:animate-slide-in animate-fade-in-down': 'md:animate-slide-out animate-fade-out-down'} fixed top-0 right-0 z-50 h-screen shadow p-4 bg-gray-100 md:w-72 w-screen dark:bg-gray-900`} aria-labelledby="drawer-navigation-label" style={{ "--delay": 0 + "s" }}>
+      <div className="flex flex-col lg:justify-between h-full">
+        <div>
+          <h5 id="drawer-navigation-label" className="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">Contents</h5>
+          <button type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" className="text-gray-500 dark:text-gray-400 hover:text-gray-900 bg-transparent hover:bg-gray-50 rounded-lg w-8 h-8 absolute top-3 right-3 inline-flex items-center justify-center dark:hover:bg-gray-800 dark:hover:text-gray-100" onClick={toggleDropdown}>
+            <IoClose size={20} />
+          </button>
+          <div className="py-2 overflow-y-auto">
+            <ul className="space-y-1 font-medium" onClick={toggleDropdown}>
+              <li>
+                <a href="#about" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800 group">
+                  <div className='icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2'>
+                    <FiInfo />
+                  </div>
+                  <span className="ms-3">About Me</span>
+                </a>
+              </li>
+              <li>
+                <a href="#education" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800 group">
+                  <div className='icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2'>
+                    <FiBookOpen />
+                  </div>
+                  <span className="flex-1 ms-3 whitespace-nowrap">Education</span>
+                </a>
+              </li>
+              <li>
+                <a href="#projects" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800 group">
+                  <div className='icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2'>
+                    <MdComputer />
+                  </div>
+                  <span className="flex-1 ms-3 whitespace-nowrap">Projects</span>
+                </a>
+              </li>
+              <li>
+                <a href="#skills" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800 group">
+                  <div className='icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2'>
+                    <GoGear />
+                  </div>
+                  <span className="flex-1 ms-3 whitespace-nowrap">Skills</span>
+                </a>
+              </li>
+              <li>
+                <a href="#facts" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800 group">
+                  <div className='icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2'>
+                    <FaRegGrinStars />
+                  </div>
+                  <span className="flex-1 ms-3 whitespace-nowrap">Fun Facts</span>
+                </a>
+              </li>
+              <li>
+                <a href="#contact" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800 group">
+                  <div className='icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2'>
+                    <FaRegEnvelope />
+                  </div>
+                  <span className="flex-1 ms-3 whitespace-nowrap">Contact</span>
+                </a>
+              </li>
             </ul>
+          </div>
         </div>
-</div>
+        <div>
+          <h5 className="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">Find me</h5>
+          <ul className="py-2 overflow-y-auto space-y-1 font-medium">
+            <li>
+              <a rel="noopener noreferrer" href="https://github.com/manosmin" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800 group" target="_blank" alt="github">
+                <div className='icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2'>
+                  <FaGithub />
+                </div>
+                <span className="ms-3">GitHub</span>
+              </a>
+            </li>
+            <li>
+              <a rel="noopener noreferrer" href="https://linkedin.com/in/manosmin" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800 group" target="_blank" alt="linkedin">
+                <div className='icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2'>
+                  <FaLinkedinIn />
+                </div>
+                <span className="flex-1 ms-3 whitespace-nowrap">LinkedIn</span>
+              </a>
+            </li>
+            <li>
+              <a rel="noopener noreferrer" href="https://www.discordapp.com/users/270397164209963009" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800 group" target="_blank" alt="discord">
+                <div className='icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2'>
+                  <FaDiscord />
+                </div>
+                <span className="flex-1 ms-3 whitespace-nowrap">Discord</span>
+              </a>
+            </li>
+            <li>
+              <a rel="noopener noreferrer" href="https://1drv.ms/b/s!AvPPFklWx5YlgcVK4G_e28UQDwI2oQ?e=e7nrZb" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800 group" target="_blank" alt="CV">
+                <div className='icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2'>
+                  <IoDocument />
+                </div>
+                <span className="flex-1 ms-3 whitespace-nowrap">CV</span>
+              </a>
+            </li>
+            <li>
+              <a rel="noopener noreferrer" href="mailto:manosmin@hotmail.com" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800 group" target="_blank" alt="e-mail">
+                <div className='icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2'>
+                  <MdAlternateEmail />
+                </div>
+                <span className="flex-1 ms-3 whitespace-nowrap">Email</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Drawer
+export default Drawer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,6 @@
-// src/components/Header.js
 import React, { useState } from 'react';
 import Drawer from './Drawer';
-import { FaGithub, FaLinkedinIn, FaDiscord } from "react-icons/fa";
-import { MdAlternateEmail } from "react-icons/md";
-import { IoMoon, IoSunny, IoDocument, IoMenu } from "react-icons/io5";
+import { IoMoon, IoSunny, IoMenu } from "react-icons/io5";
 
 const Header = ({ dark, darkModeHandler }) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -15,44 +12,28 @@ const Header = ({ dark, darkModeHandler }) => {
   };
 
   return (
-    <header className="z-40 sticky top-0 bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100 shadow p-5">
-      <h1 className="max-w-screen-xl mx-auto w-full text-4xl font-bold">
-        <a href="/#" className='inline-block animate-slide-in-left opacity-0' style={{ "--delay": 0.2 + "s" }}>Manos Minadakis</a>
-        &nbsp;
-        <small className="inline-block animate-slide-in opacity-0 font-semibold text-gray-500 dark:text-gray-400" style={{ "--delay": 0.2 + "s" }}>Computer Engineer</small>
-      </h1>
-      <nav>
-        <ul className="max-w-screen-xl mx-auto w-full flex justify-between mt-3 list-none">
-          <div className='flex animate-drop-in opacity-0' style={{ "--delay": 0.8 + "s" }}>
-            <li id="github" className="icon-container bg-gray-900 text-white dark:bg-gray-100 dark:text-gray-800 rounded-full p-2 mx-1">
-              <a rel="noopener noreferrer" href="https://www.github.com/manosmin" alt="github" target="_blank"><FaGithub size={20}/></a>
-            </li>
-            <li className="icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2 mx-1">
-              <a rel="noopener noreferrer" href="https://www.linkedin.com/in/manosmin" target="_blank" alt="linkedin"><FaLinkedinIn size={20}/></a>
-            </li>
-            <li className="icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2 mx-1">
-              <a rel="noopener noreferrer" href="https://www.discordapp.com/users/270397164209963009" target="_blank" alt="discord" ><FaDiscord size={20}/></a>
-            </li>
-            <li className="icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2 mx-1">
-              <a rel="noopener noreferrer" href="https://1drv.ms/b/s!AvPPFklWx5YlgcVK4G_e28UQDwI2oQ?e=e7nrZb" target="_blank" alt="CV" ><IoDocument size={20}/></a>
-            </li>
-            <li className="icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2 mx-1">
-              <a href="mailto:manosmin@hotmail.com" alt="email" ><MdAlternateEmail size={20}/></a>
-            </li>
-          </div>
-          <div className='flex animate-drop-in opacity-0' style={{ "--delay": 0.8 + "s" }}>
-            <li className="flex icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2 mx-1">
-              <button alt="darkmode" onClick={darkModeHandler}>
-                { !dark && <IoMoon size={20}/> }
-                { dark && <IoSunny size={20}/> }
-              </button>
-            </li>
-            <li className="flex cursor-pointer icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2 mx-1" alt='dropdown' onClick={toggleDropdown}>
-              <IoMenu size={20} />
-            </li>
-          </div>
-        </ul>
-      </nav>
+    <header className="z-40 sticky top-0 bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100 shadow p-3">
+      <div className="max-w-screen-xl mx-auto w-full flex justify-between items-center">
+        <div className="text-2xl md:text-4xl font-bold">
+          <a href="/#" className='inline-block animate-slide-in-left opacity-0' style={{ "--delay": 0.2 + "s" }}>Manos Minadakis </a>
+          <small className="md:ml-2 inline-block animate-slide-in opacity-0 font-semibold text-gray-500 dark:text-gray-400" style={{ "--delay": 0.2 + "s" }}>Computer Engineer</small>
+        </div>
+        <div>
+          <ul className="flex list-none">
+            <div className='flex animate-drop-in opacity-0' style={{ "--delay": 0.8 + "s" }}>
+              <li className="flex icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2 mx-1">
+                <button alt="darkmode" onClick={darkModeHandler}>
+                  { !dark && <IoMoon size={20}/> }
+                  { dark && <IoSunny size={20}/> }
+                </button>
+              </li>
+              <li className="flex cursor-pointer icon-container bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 rounded-full p-2 mx-1" alt='dropdown' onClick={toggleDropdown}>
+                <IoMenu size={20} />
+              </li>
+            </div>
+          </ul>
+        </div>
+      </div>
       { hasInteracted && <Drawer toggleDropdown={toggleDropdown} isOpen={dropdownOpen} /> }
     </header>
   );
